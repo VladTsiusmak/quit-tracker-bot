1d122f5efee66e7fd1821ab97f13fe84281e6a3c
Владислав <tsuva01032005@gmail.com>
2026-09-02 20:44:34 +0300

    reset database schema on startup

--- backend.py
+++ backend.py
@@ -104,6 +104,8 @@ async def get_db():
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
+        # Временно удаляем и пересоздаем таблицы для обновления схемы PostgreSQL
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
